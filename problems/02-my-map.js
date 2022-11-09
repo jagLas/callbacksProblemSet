8eac1e58564fe673d725175909dd66724663b549
jagLas <jglasher@gmail.com>
2022-11-08 23:51:07 -0500

    first two

--- problems/02-my-map.js
+++ problems/02-my-map.js
@@ -16,11 +16,21 @@ let result2 = myMap(['run', 'Forrest'], function (el) {
 console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 *******************************************************************************/
 
-let myMap = function() {
-
+let myMap = function(array, cb) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++){
+        newArray.push(cb(array[i], i, array));
+    }
+    return newArray;
 };
 
+// let result1 = myMap([100, 25, 81, 64], Math.sqrt);
+// console.log(result1);   // [ 10, 5, 9, 8 ]
 
+// let result2 = myMap(['run', 'Forrest'], function (el) {
+//     return el.toUpperCase() + '!';
+// });
+// console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 
 
 
