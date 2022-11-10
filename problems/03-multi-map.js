3c7f35f6df5faba2e522a1680c976b3ecde925be
jagLas <jglasher@gmail.com>
2022-11-10 15:30:03 -0500

    finished two problems

--- problems/03-multi-map.js
+++ problems/03-multi-map.js
@@ -21,12 +21,31 @@ let result3 = multiMap("hi", 5, function(s) {
 console.log(result3); // hi!!!!!
 *******************************************************************************/
 
-let multiMap = function() {
+let multiMap = function(value, n, cb) {
+  let i = 0;
+  while (i < n) {
+    value = cb(value);
+    i++;
+  }
+  return value;
 
 };
 
 
+// let result1 = multiMap(7, 2, function(n) {
+//   return n * 10;
+// });
+// console.log(result1); // 700
 
+// let result2 = multiMap(7, 3, function(n) {
+//     return n * 10;
+// });
+// console.log(result2); // 7000
+
+// let result3 = multiMap("hi", 5, function(s) {
+//   return s + "!";
+// });
+// console.log(result3); // hi!!!!!
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 module.exports = multiMap;
